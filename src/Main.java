e9b058b845fd194ee40aa82808eafffacad9fd5f
Chetan Patil <chetanpatil6303@gmail.com>
2025-07-10 20:01:54 +0530

    created main menu for hotel management system

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,8 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
@@ -7,7 +12,36 @@ public class Main {
         private static final String pass = "root";
 
 
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+        Scanner sc = new Scanner(System.in);
+
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+//            loading the database driver that are com.mysql.cj package
+        }
+        catch (ClassNotFoundException e){
+            System.out.println(e.getMessage());
+        }
+
+        try{
+            Connection connection = DriverManager.getConnection(url,user,pass);
+
+            while (true){
+                System.out.println();
+                System.out.println("HOTEL MANAGEMENT SYSTEM ");
+                System.out.println("1. Reserve a room");
+                System.out.println("2. View Reservations");
+                System.out.println("3. Get Room number");
+                System.out.println("4. Update Reservations");
+                System.out.println("5. Delete Reservations");
+                System.out.println("6. EXIT");
+                System.out.print("\nEnter Choice : ");
+                int choice = sc.nextInt();
+
+            }
+
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
