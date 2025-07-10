3ccc8e9936ef7e4693337d868c774e90d0cca127
Chetan Patil <chetanpatil6303@gmail.com>
2025-07-10 19:40:32 +0530

    update created database and reservation table

--- src/Database_schema/hotel_db.sql
+++ src/Database_schema/hotel_db.sql
@@ -3,3 +3,15 @@ show databases;
 create database hotel_db;
 use hotel_db;
 
+
+CREATE TABLE reservation (
+    res_id INT AUTO_INCREMENT PRIMARY KEY,
+    guest_name VARCHAR(255) NOT NULL,
+    room_no INT NOT NULL,
+    contact_no VARCHAR(12) NOT NULL,
+    resrvation_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+describe reservation;
+
+
