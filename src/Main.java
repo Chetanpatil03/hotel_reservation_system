b48b3ae1121ebd116964caa335576b6005dde79e
Chetan Patil <chetanpatil6303@gmail.com>
2025-07-10 19:50:35 +0530

    database connection crediential

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,13 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
+
+        private static final String url = "jdbc:mysql://localhost:3306/hotel_db";
+        private static final String user = "root";
+        private static final String pass = "root";
+
+
     public static void main(String[] args) {
-         
+        
     }
 }
